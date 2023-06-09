6bae250abb173a9edbf7eeafb334dabab287fa86
nabilsynfl <nabilsyah745@gmail.com>
2023-06-09 21:57:30 +0700

    Update Timer

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,32 +1,45 @@
-import React from 'react';
+"use client"
+
+import {useState, useEffect} from 'react';
 import './header.css';
 
-export async function getServerSideProps() {
-  const currentTime = new Date().toString();
+const Header = () => {
+  const [seconds,setSeconds] = useState(0);
+  const [minutes,setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
 
-  return {
-    props: {
-      currentTime
-    }
-  };
-}
+  let timer;
+  useEffect(() => {
+    timer=setInterval(() => {
+      setSeconds(seconds+1);
+      if(seconds===59){
+        setMinutes(minutes+1)
+        if(minutes===60){
+          setHours(hours+1);
+        }
+        setSeconds(0)
+      }
+    }, 1000)
 
-const Header = ({currentTime}) => {
+    return ()=> clearInterval(timer);
+  });
+
+  let showDate = new Date();
+  let getHours = showDate.getHours();
+  let getMinutes = showDate.getMinutes();
+
   return (
     <header className="header">
       <nav>
         <div className='left-side'>
           <h4><span>48</span> LEVEL</h4>
-          <div className='icon-header'>
-            <img src="../header-icon/+.svg" alt="" />
-          </div>
-          <h4><span>1,425</span> COINS AWARDED</h4>
+          <h4><span>1,425</span> VISITOR</h4>
         </div>
 
         <div className="timer">
           <h4>CREDITS</h4>
-          <h4>SERVER TIME: <span>8:42</span></h4>
-          <h4>LOCAL TIME : <span>15:42</span></h4>    
+          <h4>SERVER TIME: <span>{hours}:{minutes}:{seconds}</span></h4>
+          <h4>LOCAL TIME : <span>{getHours}:{getMinutes}</span></h4>    
         </div>
       </nav>
     </header>
