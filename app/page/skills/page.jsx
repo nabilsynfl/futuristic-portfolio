54c2af441a3dda5213d45be449a0ecc596d9c5e5
nabilsynfl <nabilsyah745@gmail.com>
2023-06-11 21:03:25 +0700

    Update component

--- app/page/skills/page.jsx
+++ app/page/skills/page.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 const Skill = () => {
   return (
     <>
+      <div class="bg-color"></div>
       <div className="container-content">
-        <div className="bg-color"></div>   
+        <div class="top left"></div>
+        <div class="top right"></div>
+        <div class="bottom right"></div>
+        <div class="bottom left"></div>
       </div>
     </>
   )
