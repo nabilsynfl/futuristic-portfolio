1ce3d97bede834d74d14b3a7d1cbf74e8d82699e
nabilsynfl <nabilsyah745@gmail.com>
2023-06-17 20:34:56 +0700

    Component Skills Updated

--- app/page/skills/page.jsx
+++ app/page/skills/page.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './skil.css';
 
 const Skill = () => {
   return (
     <>
       <div className="bg-color"></div>
       <div className="container-content">
+        <div className="tech">
+          <h1>Technologies I Used</h1>
+          <div className="container">
+            <div className="filter">
+              <label>Filter : </label>
+              <input type="checkbox" htmlFor="All"/>
+              <h4>All</h4>
+              <input type="checkbox" htmlFor="FrontEnd"/>
+              <h4>Front End</h4>
+              <input type="checkbox" htmlFor="BackEnd"/>
+              <h4>Back End</h4>
+              <input type="checkbox" htmlFor="cms" />
+              <h4>CMS</h4>
+            </div>
+          </div>
+        </div>
+
+
+
         <div className="top left"></div>
         <div className="top right"></div>
         <div className="bottom right"></div>
